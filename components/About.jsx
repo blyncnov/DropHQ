54d6ea607785ffeb8ce69463215f4a6c2a376a82
Taiwo Boluwatife <blyncnov@Taiwos-MacBook-Pro.local>
2022-07-27 13:53:18 +0100

    Restaurants:1

--- components/About.jsx
+++ components/About.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import Image from "next/image";
+
+import ImageOne from "../public/assets/images/res-1.jpeg";
+import ImageTwo from "../public/assets/images/res-2.jpeg";
 
 const About = () => {
   return (
@@ -6,8 +10,32 @@ const About = () => {
       <div className="Layout__constraint">
         <h1>Our Restaurants</h1>
         <div className="About__Grid">
-          <div className="About__Box">1</div>
-          <div className="About__Box">1</div>
+          <div className="About__Box">
+            <div className="About__Img">
+              <Image src={ImageOne} alt="restaurant" layout="responsive" />
+            </div>
+            <div className="About__Text__Description">
+              <h1>Tasty Bites Healthy Foods</h1>
+              <p>
+                Discover local, on-demand delivery or Pickup from restaurants,
+                nearby grocery and convenience stores, and more.
+              </p>
+              <button className="btn">View Restaurant</button>
+            </div>
+          </div>
+          <div className="About__Box">
+            <div className="About__Img">
+              <Image src={ImageTwo} alt="restaurant" layout="responsive" />
+            </div>
+            <div className="About__Text__Description">
+              <h1>Tasty Bites Healthy Foods</h1>
+              <p>
+                Discover local, on-demand delivery or Pickup from restaurants,
+                nearby grocery and convenience stores, and more.
+              </p>
+              <button className="btn">View Restaurant</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
