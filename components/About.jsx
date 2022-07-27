b14a7c2fa62073d627d02e915a47379e9a6dab5b
Taiwo Boluwatife <blyncnov@Taiwos-MacBook-Pro.local>
2022-07-27 14:06:57 +0100

    Restaurants:3

--- components/About.jsx
+++ components/About.jsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 
 import ImageOne from "../public/assets/images/res-1.jpeg";
 import ImageTwo from "../public/assets/images/res-2.jpeg";
+import ImageThree from "../public/assets/images/res-3.avif";
+import ImageFour from "../public/assets/images/res-4.avif";
 
 const About = () => {
   return (
@@ -15,7 +17,7 @@ const About = () => {
               <Image src={ImageOne} alt="restaurant" layout="responsive" />
             </div>
             <div className="About__Text__Description">
-              <h1>Tasty Bites Healthy Foods</h1>
+              <h1>Boripe Canteen & Store</h1>
               <p>
                 Discover local, on-demand delivery or Pickup from restaurants,
                 nearby grocery and convenience stores, and more.
@@ -28,7 +30,33 @@ const About = () => {
               <Image src={ImageTwo} alt="restaurant" layout="responsive" />
             </div>
             <div className="About__Text__Description">
-              <h1>Tasty Bites Healthy Foods</h1>
+              <h1>Hand Of God Restaurant</h1>
+              <p>
+                Discover local, on-demand delivery or Pickup from restaurants,
+                nearby grocery and convenience stores, and more.
+              </p>
+              <button className="btn">View Restaurant</button>
+            </div>
+          </div>
+          <div className="About__Box">
+            <div className="About__Img">
+              <Image src={ImageThree} alt="restaurant" layout="responsive" />
+            </div>
+            <div className="About__Text__Description">
+              <h1>Bamboo Kitchen</h1>
+              <p>
+                Discover local, on-demand delivery or Pickup from restaurants,
+                nearby grocery and convenience stores, and more.
+              </p>
+              <button className="btn">View Restaurant</button>
+            </div>
+          </div>
+          <div className="About__Box">
+            <div className="About__Img">
+              <Image src={ImageFour} alt="restaurant" layout="responsive" />
+            </div>
+            <div className="About__Text__Description">
+              <h1>Mhunis Restaurant</h1>
               <p>
                 Discover local, on-demand delivery or Pickup from restaurants,
                 nearby grocery and convenience stores, and more.
